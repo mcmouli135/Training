026e1c96831849ecfdd580a4e5b9f7cebbfbc600
mcmouli135 <mcmouli135@gmail.com>
2022-04-03 14:21:04 +0530

    phone

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,33 @@
+package org.phone;
+
+public class PhoneInfo {
+
+	public void phoneName() {
+	System.out.println("One Plus");
+
+	}
+	public void phoneMiei() {
+	System.out.println("7874568469875982745");
+
+	}
+	public void phoneCamera() {
+	System.out.println("20 Pixels");
+
+	}
+	public void phoneStorage() {
+	System.out.println("126 GB Internal Storage");
+
+	}
+	public void phoneOsName() {
+	System.out.println("Andriod 11");
+
+	}
+	public static void main(String[] args) {
+		PhoneInfo phoneInfo= new PhoneInfo();
+		phoneInfo.phoneName();
+		phoneInfo.phoneMiei();
+		phoneInfo.phoneCamera();
+		phoneInfo.phoneStorage();
+		phoneInfo.phoneOsName();
+	}
+}
