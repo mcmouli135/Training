71e16d9b4ac278cf674a9e7215fd185c7cd7989e
mcmouli135 <mcmouli135@gmail.com>
2022-04-03 14:42:25 +0530

    updated details

--- src/org/phone/PhoneInfo.java
+++ src/org/phone/PhoneInfo.java
@@ -20,7 +20,14 @@ public class PhoneInfo {
 	}
 	public void phoneOsName() {
 	System.out.println("Andriod 11");
+	
 
+	}
+	public void phoneModel() {
+		System.out.println("IPhone 12 pro max");
+	}
+	public void phoneUser() {
+		System.out.println("single user");
 	}
 	public static void main(String[] args) {
 		PhoneInfo phoneInfo= new PhoneInfo();
